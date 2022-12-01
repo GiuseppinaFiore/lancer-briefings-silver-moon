a5349dfb2d038178d708e9ede2b61949466d0182
GiuseppinaFiore <104180458+GiuseppinaFiore@users.noreply.github.com>
2022-11-30 22:54:36 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -119,7 +119,7 @@ export default {
         {
           "callsign": "SCRAP TAIL",
           "alias": "Dust",
-          "code": "//UNABLE TO RETRIEVE IDENTIFICATION INFO",
+          "code": "// !ERROR! UNABLE TO RETRIEVE IDENTIFICATION INFO!",
           "corpro": "GMS",
           "frame": "EVEREST",
           "mech": "SAMSARA, ETERNAL WHEEL OF BECOMING",
